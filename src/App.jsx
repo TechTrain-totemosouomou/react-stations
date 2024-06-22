c45d8bf415cf0c6d02728e41eb22e102c72bf566
totemosouomou <totemosouomou@users.noreply.github.com>
2024-06-23 08:09:19 +0900

    station3 clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 // DO NOT DELETE
 
 import './App.css'
+import { useState, useEffect } from 'react';
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogImageUrl, setDogImageUrl] = useState('');
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === 'success') {
+          setDogImageUrl(data.message);
+        }
+      })
+      .catch(error => console.error('Error fetching the dog image:', error));
+  }, []);
+
   return (
     <header>
-    Hello,world!</header>
-  )
+      Hello, world!
+      <img src={dogImageUrl} alt="A Random Dog" />
+    </header>
+  );
 }
