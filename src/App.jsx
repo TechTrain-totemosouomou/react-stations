f41e374068104e7451e87ce77fd6b34ef0c3a462
totemosouomou <totemosouomou@users.noreply.github.com>
2024-06-23 08:32:35 +0900

    station5 clear

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,27 @@ import { useState, useEffect } from 'react';
  * @type {() => JSX.Element}
  */
 export const App = () => {
-  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg');
+  const [dogImageUrl, setDogImageUrl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg');
 
   useEffect(() => {
-    fetch('https://dog.ceo/api/breeds/image/random')
-      .then(response => response.json())
-      .then(data => {
-        if (data.status === 'success') {
-          setDogUrl(data.message);
-        }
-      })
-      .catch(error => console.error('Error fetching the dog image:', error));
   }, []);
+
+  const setDogUrl = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === 'success') {
+        setDogImageUrl(data.message);
+      }
+    })
+    .catch(error => console.error('Error fetching the dog image:', error));
+  }
 
   return (
     <header>
       Hello, world!
-      <img src={dogUrl} alt="A Random Dog" />
+      <img src={dogImageUrl} alt="A Random Dog" />
+      <button onClick={setDogUrl}>更新</button>
     </header>
   );
 }
